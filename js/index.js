43ae387ceee39ba7075144dbb3ed274f6b309b25
Pong <Pong.On@infocast.com.hk>
2016-02-17 17:01:42 +0800

    1. more test on angular theme, directive, etc...

--- js/index.js
+++ js/index.js
@@ -1,12 +1,37 @@
 angular.module('docsSimpleDirective', [])
-  .controller('Controller', ['$scope', function($scope) {
+  .controller('outerController', ['$scope', function($scope) {
     $scope.customer = {
       name: 'Naomi',
-      address: '1600 Amphitheatre'
+      address: '1600 Amphitheatre',
+      outerScope: 'outer'
     };
+    $scope.inputText = 123;
+    $scope.styleSheet = 'whiteTheme.css';
   }])
   .directive('myCustomer', function() {
     return {
       template: 'Name: {{customer.name}} Address: {{customer.address}}'
     };
+  })
+  .directive('myTemplate', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'template/template.html',
+      scope: {
+        input: '='
+      }
+    };
   });
+// angular.module('docsRestrictDirective', [])
+//   .controller('Controller', ['$scope', function($scope) {
+//     $scope.customer = {
+//       name: 'Naomi',
+//       address: '1600 Amphitheatre'
+//     };
+//   }])
+//   .directive('myCustomer', function() {
+//     return {
+//       restrict: 'E',
+//       templateUrl: 'template/template.html'
+//     };
+//   });
